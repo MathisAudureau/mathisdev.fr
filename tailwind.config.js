14df02ff8ef0cd1191b0fe8c4090efcc82ddf9b2
Mathïs <87903769+MathisAudureau@users.noreply.github.com>
2022-05-31 11:15:28 +0200

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  important: true,
+  darkMode: "class",
+  i18n: {
+    locales: ["en-US"],
+    defaultLocale: "en-US",
+  },
+  purge: {
+    content: ["./src/*{html,js}"],
+  },
+  theme: {
+    extend: {
+      backgroundImage: (theme) => ({
+        check: "url('/icons/check.svg')",
+        landscape: "url('/images/landscape/2.jpg')",
+      }),
+    },
+  },
+  variants: {
+    extend: {
+      backgroundColor: ["checked"],
+      borderColor: ["checked"],
+      inset: ["checked"],
+      zIndex: ["hover", "active"],
+    },
+  },
+  plugins: [],
+  future: {
+    purgeLayersByDefault: true,
+  },
+};
